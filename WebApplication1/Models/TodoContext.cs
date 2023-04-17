42090b6a7b163ae01310aa22023c289792ea65ab
NikitochkaHrum <ponkin.nikitka@mail.ru>
2023-04-17 08:31:11 +0500

    Добавьте файлы проекта.

---
+++ WebApplication1/Models/TodoContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Models
+{
+    public class TodoContext : DbContext
+    {
+        public DbSet<TodoItem> TodoItems { get; set; }
+
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+
+}
